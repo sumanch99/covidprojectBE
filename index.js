7c2bf38ffd5ead8469ff3a1c6c0c8880adecd7fa
sumanch99 <sch.suman@gmail.com>
2020-12-05 21:24:10 +0530

    MongoDB URI changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const users = require('./routes/users')
 const doctors = require('./routes/doctors')
 
 // connect to database
-mongoose.connect('mongodb+srv://sch:2311@cluster0.p2men.mongodb.net/covapp?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false})
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/covapp',{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false})
     .then(()=> console.log('Connected MongoDB'))
     .catch(() => console.error('Could Not Connect'));
 
